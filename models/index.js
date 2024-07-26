e10adff312bf0a6b450726f6d20e34a536860979
rmadhn <ahmadrmdh93@gmail.com>
2024-07-26 21:03:13 +0700

    perbaikan

--- models/index.js
+++ models/index.js
@@ -1,16 +1,21 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Menggunakan variabel environment untuk konfigurasi database
-const sequelize = new Sequelize({
-  host: process.env.HOST,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  dialect: "mysql",
-  dialectModule: require("mysql2"),
-  benchmark: true,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+    port: process.env.DB_PORT || 3306, // Default port for MySQL
+  }
+);
+
+module.exports = {
+  sequelize,
+};
 
 // Mengimpor model dan menghubungkan dengan instance Sequelize
 const Category = require("./category")(sequelize);
